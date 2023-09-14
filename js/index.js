c713f7eedeb692bf9c9450dcbb85e01d8bc58229
Kadyrberdi <knurberdiyev@gmail.com>
2023-09-14 18:13:36 +0500

    add grid section

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
   // options
   itemSelector: '.grid-item',
-  columnWidth: 160
+  columnWidth: 160,
+  gutter: 10
 });
 
 // element argument can be a selector string

f4537157d68e29be8460eb5535a5944a3511f633
Kadyrberdi <knurberdiyev@gmail.com>
2023-12-28 18:05:33 +0500

    add sign up form

--- js/index.js
+++ js/index.js
@@ -11,3 +11,50 @@ var msnry = new Masonry(elem, {
 var msnry = new Masonry('.grid', {
   // options
 });
+
+//SHARE MODAL
+
+const viewBtn = document.querySelectorAll(".view-modal"),
+      popup = document.querySelector(".popup"),
+      close = popup.querySelectorAll(".close"),
+      field = popup.querySelector(".field"),
+      input = field.querySelector("input"),
+      copy = field.querySelector("button");
+
+viewBtn.forEach(openPopup);
+close.forEach(closePopup);
+
+/* viewBtn.onclick = ()=>{
+  popup.classList.toggle("show");
+} */
+
+copy.onclick = ()=>{
+  input.select(); //select input value
+  if(document.execCommand("copy")){ //if the selected text copy
+    field.classList.add("active");
+    copy.innerText = "Copied";
+    setTimeout(()=>{
+      window.getSelection().removeAllRanges(); //remove selection from document
+      field.classList.remove("active");
+      copy.innerText = "Copy";
+    }, 3000);
+  }
+}
+
+
+
+function openPopup(viewBtn) {
+  viewBtn.onclick = ()=>{
+    popup.classList.toggle("show");
+    a_links = popup.getElementsByClassName("social-media")
+    /* a_links[0].href = "/#####"; */
+  }
+}
+function closePopup(viewBtn) {
+  close.onclick = ()=>{
+    viewBtn[0].click();
+    popup.classList.toggle("show");
+    //card[0].click();
+  }
+}
+
--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
+
+  //Dörede basaňda şu aşakdaky modal akno işleýär
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
-
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
   }
-
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
@@ -26,23 +26,55 @@ $(document).ready(function () {
       modalDialog.removeClass("modal__dialog--visible");
     }
   });
-  $(".form").each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      messages: {
-        name: {
-          required: "*Please specify your name",
-          minlength: "Please enter at least 2 characters.",
-        },
-        phone: {
-          required: "*Please specify your phone",
-          minlength: "*Please enter at least 10 characters.",
-        },
-        email: {
-          required: "*We need your email address to contact with you",
-          email: "*Your email address must be in the format of name@domain.com",
-        },
-      },
-    });
+
+  //Suratyň gyrasyna basaňda şu aşakdaky modal akno işlemeli
+  //var contentButton = $("[data-toggle=data_content]");
+  var contentButton = $(".grid-item");
+  var closeContentButton = $(".data_content__close");
+  contentButton.on("click", openContent);
+  closeContentButton.on("click", closeContent);
+  function openContent() {
+    console.log('salam');
+    var contentOverlay = $(".data_content__overlay");
+    var contentDialog = $(".data_content__dialog");
+    contentOverlay.addClass("data_content__overlay--visible");
+    contentDialog.addClass("data_content__dialog--visible");
+  }
+  function closeContent(event) {
+    event.preventDefault();
+    var contentOverlay = $(".data_content__overlay");
+    var contentDialog = $(".data_content__dialog");
+    contentOverlay.removeClass("data_content__overlay--visible");
+    contentDialog.removeClass("data_content__dialog--visible");
+  }
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      var contentOverlay = $(".data_content__overlay");
+      var contentDialog = $(".data_content__dialog");
+      contentOverlay.removeClass("data_content__overlay--visible");
+      contentDialog.removeClass("data_content__dialog--visible");
+    }
   });
+
+
+  //mataň görnüşini saýlamak
+  $(".custom-select-wrapper-colour").on("click", function (e) {
+    e.preventDefault();
+    $(".select-options-wrapper").toggle();
+  });
+
+  $(".select-options-wrapper ul li").on("click", function (e) {
+    e.preventDefault();
+    console.log($(this).text());
+    $(this).closest("ul").find("li").removeClass("--selected");
+    $(this).addClass("--selected");
+    $(".selected-option span").html($(this).text());
+  });
+
+  $(document).on("click", function (e) {
+    if ($(e.target).closest(".custom-select-wrapper-colour").length === 0) {
+      $(".select-options-wrapper").hide();
+    }
+  });
+
 });
